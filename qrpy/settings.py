c5ec86934bc07c741c170bb6813e0c45833144d9
Truls Henriksson <trulshenriksson@gmail.com>
2026-06-16 10:36:34 +0200

    Added temporary settings

--- qrpy/settings.py
+++ qrpy/settings.py
@@ -1,8 +1,9 @@
 from qrpy.custom_types import *
 from dataclasses import dataclass
+from contextlib import contextmanager
 
 
-__all__ = ["SETTINGS"]
+__all__ = ["SETTINGS", "temporary_settings"]
 
 
 @dataclass
@@ -11,3 +12,16 @@ class Settings:
 
 
 SETTINGS = Settings()
+
+
+@contextmanager
+def temporary_settings(**kwargs):
+    # Copy the initial settings
+    settings_before = dict(SETTINGS.__dict__)
+    try:
+        for key, value in kwargs.items():
+            setattr(SETTINGS, key, value)
+        yield SETTINGS
+    finally:
+        for key, value in settings_before.items():
+            setattr(SETTINGS, key, value)
